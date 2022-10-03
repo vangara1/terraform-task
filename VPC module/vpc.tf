af34fca6a76de5c7393f859f6b1cdbc7269c5547
sandeep <vangarasandeep1996@gmail.com>
2022-10-03 15:22:49 +0530

    nat gateway

--- VPC module/vpc.tf
+++ VPC module/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = var.NAME
   cidr = var.CIDR
   azs                = var.AZ
-#  private_subnets    = var.PVT-SUBNET
+  private_subnets    = var.PVT-SUBNET
   public_subnets     = var.SUBNET
   enable_nat_gateway = true
   enable_vpn_gateway = true

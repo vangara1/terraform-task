eb67c93a5dcbc6b77f7f7827aef2212f8f2800fe
sandeep <vangarasandeep1996@gmail.com>
2022-10-03 15:44:00 +0530

    nat gateway

--- VPC module/sg.tf
+++ VPC module/sg.tf
@@ -3,7 +3,7 @@ module "sg" {
 
   name        = var.NAME
   description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.id
+  vpc_id      = [module.vpc.vpc_id]
 
   ingress_cidr_blocks      = var.CIDR
   ingress_rules            = ["https-443-tcp"]

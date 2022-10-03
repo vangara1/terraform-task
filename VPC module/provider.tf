1b3f4a4643e3209c823658eeb7f7a77739a18428
sandeep <vangarasandeep1996@gmail.com>
2022-10-03 15:38:57 +0530

    nat gateway

--- VPC module/provider.tf
+++ VPC module/provider.tf
@@ -1,6 +1,9 @@
-providers {
-  aws = {
-    source  = "hashicorp/aws"
-    version = "~> 4.4.0"
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "module-use"
+    }
   }
 }

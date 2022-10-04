4a88e5a36367d7d17e982abd99de2555bc620ac7
sandeep <vangarasandeep1996@gmail.com>
2022-10-04 16:19:32 +0530

    task 1

--- task-3/variables.tf
+++ task-3/variables.tf
@@ -2,9 +2,7 @@ variable "NAME" {}
 variable "CIDR" {}
 variable "AZ" {}
 variable "SUBNET" {}
-variable "PVT-SUBNET" {}
 variable"spot_type" {}
 variable "spot_behavior" {}
 variable "ami" {}
 variable"instance_type"{}
-variable "day" {}

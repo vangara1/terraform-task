cb4501a7b992065ab093910c2f266911a70a6d04
sandeep <vangarasandeep1996@gmail.com>
2022-10-05 01:41:09 +0530

    storage class

--- task-4-EKS/storageclass.tf
+++ task-4-EKS/storageclass.tf
@@ -17,18 +17,14 @@ resource "kubernetes_storage_class" "storage" {
   metadata {
     name = "storage"
   }
-  storage_provisioner = "ebs.csi.aws.com"
+
+  storage_provisioner = "kubernetes.io/gce-pd"
   reclaim_policy      = "Retain"
   parameters = {
-    type = "gp3"
-    fsType: "ext4"
-    encrypted: "true"
+    type = "pd-standard"
   }
-  allow_volume_expansion = true
-  volume_binding_mode = "Immediate"
+  mount_options = ["file_mode=0700", "dir_mode=0777", "mfsymlinks", "uid=1000", "gid=1000", "nobrl", "cache=none"]
 }
-
-
 #
 #resource "null_resource" "k8s_storage_class_patch" {
 #  depends_on = [kubernetes_storage_class.expandable-storage]

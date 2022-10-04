3e79acf8f5dfcca65ab915db2dc77f7f8442feda
sandeep <vangarasandeep1996@gmail.com>
2022-10-04 13:49:41 +0530

    task 1

---
+++ task-1/ec2.tf
@@ -0,0 +1,25 @@
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+
+  name = var.NAME
+
+  create_spot_instance                = true
+  spot_type                           = var.spot_type
+  spot_instance_interruption_behavior = var.spot_behavior
+  associate_public_ip_address         = true
+  ami                                 = var.ami
+  instance_type                       = var.instance_type
+  key_name                            = var.NAME
+  monitoring                          = true
+  iam_instance_profile                = aws_iam_instance_profile.role_profile.name
+  vpc_security_group_ids              = [module.sg.security_group_id]
+  subnet_id                           = module.vpc.public_subnets[0]
+  #  role                                = module.iam_role.id
+
+  tags = {
+    name = var.NAME
+  }
+}
+
+

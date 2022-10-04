cfb46714749389ba3f71b42b16570e8ae32a070d
sandeep <vangarasandeep1996@gmail.com>
2022-10-04 12:06:21 +0530

    nat gateway

--- VPC module/role.tf
+++ VPC module/role.tf
@@ -2,8 +2,7 @@ module "iam_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
 
   trusted_role_arns = [
-    "arn:aws:iam::307990089504:root",
-    "arn:aws:iam::835367859851:user/anton",
+    "arn:aws:iam::307990089504:root"
   ]
 
   create_role = true
@@ -14,8 +13,8 @@ module "iam_role" {
   custom_role_policy_arns = [module.iam_policy.id]
   number_of_custom_role_policy_arns = 1
 }
-
-resource "aws_iam_role_policy_attachment" "wave-role" {
-  role       = module.iam_role.role_name
-  policy_arn = module.iam_policy.name
-}
+#
+#resource "aws_iam_role_policy_attachment" "wave-role" {
+#  role       = module.iam_role.role_name
+#  policy_arn = module.iam_policy.name
+#}

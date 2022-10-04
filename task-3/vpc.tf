6a6b52e32eff95b8fdaf50347e85e076a4679d31
sandeep <vangarasandeep1996@gmail.com>
2022-10-04 16:17:10 +0530

    task 1

--- task-3/vpc.tf
+++ task-3/vpc.tf
@@ -4,7 +4,6 @@ module "vpc" {
   name = "${var.NAME}-vpc"
   cidr = var.CIDR
   azs                = var.AZ
-  private_subnets    = var.PVT-SUBNET
   public_subnets     = var.SUBNET
   enable_nat_gateway = true
   enable_vpn_gateway = true

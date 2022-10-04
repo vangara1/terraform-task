511d7bbfa5de8faee965465c6a7b82b68cba3323
sandeep <vangarasandeep1996@gmail.com>
2022-10-04 12:17:34 +0530

    nat gateway

--- VPC module/backend.tf
+++ VPC module/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "${var.NAME}-terraform-backend"
+    bucket = "wave-cycle-terraform-backend"
     key    = "wave-cycle/backend/data"
     region = "us-east-1"
   }

12c1b48b2e9c73c578c0d45af3ea4fb04b5c8fca
sandeep <vangarasandeep1996@gmail.com>
2022-10-04 13:45:59 +0530

    task 1

--- task-1/backend.tf
+++ task-1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terra-07009"
-    key    = "/task-1/wave-cycle/backend/data"
+    key    = "task-1/wave-cycle/backend/data"
     region = "us-east-1"
   }
 }

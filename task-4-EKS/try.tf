244f2c1a0eefebc6b7d062288042ff91e75b7966
sandeep <vangarasandeep1996@gmail.com>
2022-10-05 16:11:08 +0530

    eks

--- task-4-EKS/try.tf
+++ task-4-EKS/try.tf
@@ -13,15 +13,17 @@ module "eks" {
       resolve_conflicts = "OVERWRITE"
     }
     kube-proxy = {}
-    vpc-cni = {
+    vpc-cni    = {
       resolve_conflicts = "OVERWRITE"
     }
   }
 
-  cluster_encryption_config = [{
-    provider_key_arn = "arn:aws:kms:eu-west-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
-    resources        = ["secrets"]
-  }]
+  cluster_encryption_config = [
+    {
+      provider_key_arn = "arn:aws:kms:eu-west-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+      resources        = ["secrets"]
+    }
+  ]
 
   vpc_id     = "vpc-1234556abcdef"
   subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
@@ -30,7 +32,7 @@ module "eks" {
   self_managed_node_group_defaults = {
     instance_type                          = "m6i.large"
     update_launch_template_default_version = true
-    iam_role_additional_policies = [
+    iam_role_additional_policies           = [
       "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     ]
   }
@@ -42,7 +44,7 @@ module "eks" {
       desired_size = 2
 
       use_mixed_instances_policy = true
-      mixed_instances_policy = {
+      mixed_instances_policy     = {
         instances_distribution = {
           on_demand_base_capacity                  = 0
           on_demand_percentage_above_base_capacity = 10
@@ -62,3 +64,4 @@ module "eks" {
       }
     }
   }
+}

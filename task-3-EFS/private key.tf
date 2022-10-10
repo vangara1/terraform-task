f5bd49e7840958e9e41fb309847e9459d4afeb64
sandeep <vangarasandeep1996@gmail.com>
2022-10-10 19:51:22 +0530

    example file

--- task-3-EFS/private key.tf
+++ task-3-EFS/private key.tf
@@ -6,7 +6,7 @@ module "key_pair" {
   source = "terraform-aws-modules/key-pair/aws"
 
   key_name = var.NAME
-  public_key = trimspace(tls_private_key.key.public_key_openssh)
+  public_key = tls_private_key.key.public_key_openssh
 }
 
 resource "null_resource" "key-wave" {

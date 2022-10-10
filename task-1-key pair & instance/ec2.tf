4e0a6d23df76124a5a86686051df38ab89ba5d4e
sandeep <vangarasandeep1996@gmail.com>
2022-10-11 01:39:44 +0530

    example file

--- task-1-key pair & instance/ec2.tf
+++ task-1-key pair & instance/ec2.tf
@@ -7,7 +7,7 @@ module "ec2_instance" {
   associate_public_ip_address         = true
   ami                                 = var.ami
   instance_type                       = var.instance_type
-  key_name                            = null_resource.key-wave.id
+  key_name                            = tls_private_key.wave-key.id
   monitoring                          = true
   vpc_security_group_ids              = [module.sg.security_group_id]
   subnet_id                           = module.vpc.public_subnets[0]

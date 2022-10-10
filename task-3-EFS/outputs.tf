fed00e402fb3abf14d8b5a5fae4558c51d0a510b
sandeep <vangarasandeep1996@gmail.com>
2022-10-10 20:14:56 +0530

    example file

--- task-3-EFS/outputs.tf
+++ task-3-EFS/outputs.tf
@@ -1,6 +1,11 @@
 output "vpc_public_subnets" {
   value       = module.vpc.public_subnets
 }
+output "private_key" {
+  value     = tls_private_key.key.private_key_pem
+  sensitive = true
+}
+
 
 #output "iam_role" {
 #  value = module.iam_role.arn

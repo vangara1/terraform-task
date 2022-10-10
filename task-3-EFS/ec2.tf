a9edf89328116f45796e0f17b8790967b8a81b37
sandeep <vangarasandeep1996@gmail.com>
2022-10-10 19:37:06 +0530

    example file

--- task-3-EFS/ec2.tf
+++ task-3-EFS/ec2.tf
@@ -7,13 +7,14 @@ module "ec2_instance" {
   spot_type                           = var.spot_type
   spot_instance_interruption_behavior = var.spot_behavior
   associate_public_ip_address         = true
+  key_name                            = var.NAME
   ami                                 = var.ami
   instance_type                       = var.instance_type
   monitoring                          = true
   vpc_security_group_ids              = [module.sg.security_group_id]
   subnet_id                           = module.vpc.public_subnets[0]
 
-  tags  = {
+  tags = {
     name = "${var.NAME}-ec2"
   }
 }
---
+++ task-3-EFS/private key.tf
@@ -0,0 +1,19 @@
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name = var.NAME
+  public_key = trimspace(tls_private_key.key.public_key_openssh)
+}
+
+resource "null_resource" "key-wave" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      sudo echo '${tls_private_key.key.private_key_pem}' > ./'${var.NAME}'.pem
+      sudo chmod 400 ./'${var.NAME}'.pem
+    EOT
+  }
+}

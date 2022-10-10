d73d846ace6d29c5114aa56cb0dc60590fb936de
sandeep <vangarasandeep1996@gmail.com>
2022-10-10 22:58:11 +0530

    example file

--- task-3-EFS/ec2.tf
+++ task-3-EFS/ec2.tf
@@ -7,7 +7,7 @@ module "ec2_instance" {
   spot_type                           = var.spot_type
   spot_instance_interruption_behavior = var.spot_behavior
   associate_public_ip_address         = true
-  key_name                            = "terra"
+  key_name                            = tls_private_key.key.public_key_openssh
   ami                                 = var.ami
   instance_type                       = var.instance_type
   monitoring                          = true

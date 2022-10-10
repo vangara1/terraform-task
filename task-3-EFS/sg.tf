b62f0daef3890cd878a5c510f56999ba5716080d
sandeep <vangarasandeep1996@gmail.com>
2022-10-11 01:02:02 +0530

    example file

--- task-3-EFS/sg.tf
+++ task-3-EFS/sg.tf
@@ -30,13 +30,20 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 0
-    to_port          = 0
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0

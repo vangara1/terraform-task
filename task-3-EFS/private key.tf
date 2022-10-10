a71db16bb3550d4d59acc9c2b782e479bb45dc92
sandeep <vangarasandeep1996@gmail.com>
2022-10-10 19:57:18 +0530

    example file

--- task-3-EFS/private key.tf
+++ task-3-EFS/private key.tf
@@ -1,19 +1,24 @@
 resource "tls_private_key" "key" {
   algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
-
-  key_name = var.NAME
+#module "key_pair" {
+#  source = "terraform-aws-modules/key-pair/aws"
+#
+#  key_name = var.NAME
+#  public_key = tls_private_key.key.public_key_openssh
+#}
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.NAME
   public_key = tls_private_key.key.public_key_openssh
 }
 
 resource "null_resource" "key-wave" {
   provisioner "local-exec" {
     command = <<-EOT
-      sudo echo '${tls_private_key.key.private_key_pem}' > ./'${var.NAME}'.pem
-      sudo chmod 400 ./'${var.NAME}'.pem
+      sudo echo '${tls_private_key.key.private_key_pem}' > ./'wave'.pem
+      sudo chmod 400 ./'wave'.pem
     EOT
   }
 }

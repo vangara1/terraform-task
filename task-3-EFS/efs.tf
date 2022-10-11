62858dcb21ee790cc6148512175453d35b921ecb
sandeep <vangarasandeep1996@gmail.com>
2022-10-11 13:15:20 +0530

    example file

--- task-3-EFS/efs.tf
+++ task-3-EFS/efs.tf
@@ -17,3 +17,26 @@ module "efs" {
   subnets                 = module.vpc.public_subnets
   vpc_id                  = module.vpc.vpc_id
 }
+#resource "aws_efs_mount_target" "mount" {
+#  file_system_id = module.efs.id
+#  subnet_id      = module.vpc.s
+#  security_groups = [aws_security_group.ec2_security_group.id]
+#}
+
+resource "aws_efs_access_point" "access-point" {
+  file_system_id = module.efs.id
+
+  posix_user {
+    gid = 1000
+    uid = 1000
+  }
+
+  root_directory {
+    path = "/access"
+    creation_info {
+      owner_gid   = 1000
+      owner_uid   = 1000
+      permissions = "0777"
+    }
+  }
+}

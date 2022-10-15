189f8b4365cabd7bff3139f278ee266446476130
sandeep <vangarasandeep1996@gmail.com>
2022-10-16 02:44:07 +0530

    eks

--- task-4-EKS/eks.tf
+++ task-4-EKS/eks.tf
@@ -8,9 +8,9 @@ module "eks" {
   subnet_ids                      = module.vpc.private_subnets
 #  node_security_group_id          = module.sg.security_group_id
   eks_managed_node_group_defaults = {
-    #    ami_type                              = "AL2_x86_64"
-    attach_cluster_primary_security_group = true
-    create_security_group                 = false
+        ami_type                              = "AL2_x86_64"
+#    attach_cluster_primary_security_group = true
+#    create_security_group                 = false
   }
   eks_managed_node_groups = {
     ONE = {

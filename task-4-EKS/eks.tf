1b187e0e5fb202a6f6dfdd9bf2c4a51c7b6c0446
sandeep <vangarasandeep1996@gmail.com>
2022-10-15 11:45:47 +0530

    eks

--- task-4-EKS/eks.tf
+++ task-4-EKS/eks.tf
@@ -2,21 +2,21 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.26.6"
 
-  cluster_name    = var.NAME
-  cluster_version = "1.22"
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  cluster_name                    = var.NAME
+  cluster_version                 = "1.22"
+  vpc_id                          = module.vpc.vpc_id
+  subnet_ids                      = module.vpc.private_subnets
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type                              = "AL2_x86_64"
     attach_cluster_primary_security_group = true
-    create_security_group = false
+    create_security_group                 = false
   }
   eks_managed_node_groups = {
-      name = "${var.NAME}-group"
-      instance_types = ["t3.small"]
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-      vpc_security_group_ids = [module.sg.security_group_id]
+    name                   = "${var.NAME}-group"
+    instance_types         = ["t3.small"]
+    min_size               = 1
+    max_size               = 3
+    desired_size           = 2
+    vpc_security_group_ids = [module.sg.security_group_id]
   }
 }

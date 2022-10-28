f777a45487cb4062ac42b74dbc6d8fb079558e67
vangara1 <vangarasandeep1996@gmail.com>
2022-10-28 23:10:02 +0530

    test run

--- new-tasks/vpc.tf
+++ new-tasks/vpc.tf
@@ -1,20 +1,22 @@
-data "aws_region" "current" {}
+#data "aws_region" "current" {}
 
 locals {
   name            = "EKS-test"
   cidr            = "10.0.0.0/16"
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  azs             = formatlist("${data.aws_region.current.name}%s", ["a", "b", "c"])
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  #  azs             = formatlist("${data.aws_region.current.name}%s", ["a", "b", "c"])
 }
 
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
+  count                  = length(local.private_subnets, local.public_subnets, local.azs)
   name                   = "${local.name}-vpc"
-  azs                    = local.azs
+  azs                    = element(local.azs, count.index)
   cidr                   = local.cidr
-  private_subnets        = local.private_subnets
-  public_subnets         = local.public_subnets
+  private_subnets        = element(local.private_subnets, count.index)
+  public_subnets         = element(local.public_subnets, count.index)
   enable_dns_hostnames   = true
   enable_dns_support     = true
   enable_nat_gateway     = true

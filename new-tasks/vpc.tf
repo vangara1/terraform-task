744036e43bb563518fbeb06a21ee08f8fe4c6237
vangara1 <vangarasandeep1996@gmail.com>
2022-10-28 20:08:17 +0530

    tasks

---
+++ new-tasks/vpc.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = data.aws_vpc.selected.id
+  availability_zone = "us-east-2"
+  cidr_block        = "16.0.0.0"
+}
+
+
+

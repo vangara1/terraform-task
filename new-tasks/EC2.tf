d17930436ea07b3a12c6e5b68761b5916c5a8f15
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 01:00:15 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "instance" {
   associate_public_ip_address = true
   availability_zone           = var.az[0]
   key_name                    = "fresh"
-  subnet_id                   = aws_subnet.pub_subnet.id[0]
+  subnet_id                   = [aws_subnet.pub_subnet.id[0]]
   vpc_security_group_ids      = [aws_vpc.vpc.id]
 }

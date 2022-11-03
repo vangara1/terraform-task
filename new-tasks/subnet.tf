a520590422791c5ad76c9a6d230de37af0093dfa
vangara1 <vangarasandeep1996@gmail.com>
2022-11-03 23:47:46 +0530

    test run

--- new-tasks/subnet.tf
+++ new-tasks/subnet.tf
@@ -1,11 +1,12 @@
 resource "aws_subnet" "main" {
   vpc_id                                         = aws_vpc.main.id
+  count                                          = length(var.public_subnet)
   cidr_block                                     = element(var.public_subnet, count.index )
   availability_zone                              = element(var.az, count.index)
   enable_resource_name_dns_aaaa_record_on_launch = true
 
   tags = {
-    Name = "${varNAME}-subnet"
+    Name = "${var.NAME}-subnet"
   }
 }
 

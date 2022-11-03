b1b5321f69375812558ac6e7ef9a5db55454097f
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 01:13:37 +0530

    test run

---
+++ new-tasks/output.tf
@@ -0,0 +1,24 @@
+output "VPC_ID" {
+  value = aws_vpc.vpc.id
+}
+
+output "PRIVATE_SUBNET_IDS" {
+  value = aws_subnet.pvt-subnet.*.id
+}
+
+output "PUBLIC_SUBNET_IDS" {
+  value = aws_subnet.pub_subnet.*.id
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+  value = aws_subnet.pvt-subnet.*.cidr_block
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+  value = aws_subnet.pub_subnet.*.cidr_block
+}
+
+#
+#output "INTERNAL_HOSTEDZONE_ID" {
+#  value = var.INTERNAL_HOSTEDZONE_ID
+#}
--- new-tasks/terraform.tfvars
+++ new-tasks/terraform.tfvars
@@ -1,5 +1,5 @@
-CIDR          = "16.0.0.0/16"
-NAME          = "OnePlus"
-public_subnet = ["16.0.0.0/19", "16.0.32.0/19", "16.0.64.0/19"]
+CIDR           = "16.0.0.0/16"
+NAME           = "OnePlus"
+public_subnet  = ["16.0.0.0/19", "16.0.32.0/19", "16.0.64.0/19"]
 private_subnet = ["16.0.224.0/21", "16.0.232.0/21", "16.0.240.0/21"]
-az            = ["us-east-2a", "us-east-2b", "us-east-2c"]
+az             = ["us-east-2a", "us-east-2b", "us-east-2c"]

a9fa5a1bb54aebe3670f93e12e107a5b00caba78
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 00:27:49 +0530

    test run

--- new-tasks/subnet.tf
+++ new-tasks/subnet.tf
@@ -1,10 +1,9 @@
 resource "aws_subnet" "pub_subnet" {
   vpc_id                                         = aws_vpc.vpc.id
   count                                          = length(var.public_subnet)
-  cidr_block                                     = element(var.public_subnet, count.index )
+  cidr_block                                     = element(var.public_subnet, count.index)
   availability_zone                              = element(var.az, count.index)
   enable_resource_name_dns_a_record_on_launch = true
-
   tags = {
     Name = "${var.NAME}-pub-subnet"
   }
@@ -13,7 +12,7 @@ resource "aws_subnet" "pub_subnet" {
 resource "aws_subnet" "pvt-subnet" {
   vpc_id                                         = aws_vpc.vpc.id
   count                                          = length(var.private_subnet)
-  cidr_block                                     = element(var.private_subnet, count.index )
+  cidr_block                                     = element(var.private_subnet, count.index)
   availability_zone                              = element(var.az, count.index)
   enable_resource_name_dns_a_record_on_launch = true
 

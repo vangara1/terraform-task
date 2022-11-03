594e4860524f91a0e62dc04f2b5d9f84eee9fb51
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 00:11:05 +0530

    test run

--- new-tasks/route table.tf
+++ new-tasks/route table.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = var.CIDR
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 

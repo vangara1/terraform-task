4007106ae63c8936a9b970b3e3452542dc3a4fb1
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 00:13:49 +0530

    test run

---
+++ new-tasks/sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "sg" {
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.NAME}-sg"
+  }
+}

35190ca92241132e68aab54e99cae072e94d1c73
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 01:20:09 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -1,9 +1,9 @@
-#resource "aws_instance" "instance" {
-#  ami                         = "ami-05a36e1502605b4aa"
-#  instance_type               = "t2.micro"
-#  associate_public_ip_address = true
-#  key_name                    = "fresh"
-#  vpc_security_group_ids      = [aws_vpc.vpc.id]
-#  subnet_id                   = aws_subnet.pub_subnet.id
-#
-#}
+resource "aws_instance" "instance" {
+  ami                         = "ami-05a36e1502605b4aa"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = "fresh"
+  vpc_security_group_ids      = [aws_vpc.vpc.id]
+  subnet_id                   = aws_subnet.pub_subnet[0]
+
+}

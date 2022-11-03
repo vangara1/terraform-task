48e59fdd5e76843f8f3ac6fcf7e87dbe1d0b9060
vangara1 <vangarasandeep1996@gmail.com>
2022-11-03 23:50:41 +0530

    test run

--- new-tasks/subnet.tf
+++ new-tasks/subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "main" {
   count                                          = length(var.public_subnet)
   cidr_block                                     = element(var.public_subnet, count.index )
   availability_zone                              = element(var.az, count.index)
-  enable_resource_name_dns_aaaa_record_on_launch = true
+  enable_resource_name_dns_a_record_on_launch = true
 
   tags = {
     Name = "${var.NAME}-subnet"

d346b5c478e469f0be0c9886ce05017d137d8030
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 00:49:00 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami                         = "ami-05a36e1502605b4aa"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  availability_zone           = var.az
+  availability_zone           = var.az[0]
   key_name                    = "fresh"
   security_groups             = [aws_security_group.sg.id]
   vpc_security_group_ids      = [aws_vpc.vpc.id]

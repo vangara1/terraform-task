7c7e25e7e0325b39e5f497e761815e6881da5103
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 15:39:01 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -35,20 +35,23 @@ resource "aws_key_pair" "my_key" {
 }
 
 resource "null_resource" "kubernetes" {
-  provisioner "remote-exec" {
-    inline = ["cloud-init status --wait"]
+#  provisioner "remote-exec" {
+#    inline = ["cloud-init status --wait"]
+#  }
+  triggers = {
+    trigger = aws_instance.instance.public_ip
   }
     provisioner "local-exec" {
       command = "/bin/bash setup.sh"
     }
   # Login to the centos-user with the aws key.
-  connection {
-    type        = "ssh"
-    user        = "centos"
-    password    = ""
-    private_key = tls_private_key.key.private_key_pem
-    host        = aws_instance.instance.public_ip
-  }
+#  connection {
+#    type        = "ssh"
+#    user        = "centos"
+#    password    = ""
+#    private_key = tls_private_key.key.private_key_pem
+#    host        = aws_instance.instance.public_ip
+#  }
 }
 
 resource "aws_ssm_document" "cloud_init_wait" {

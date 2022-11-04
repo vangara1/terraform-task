2462724c464c17300cab6651c124b38698881e60
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 14:57:06 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -33,4 +33,23 @@ resource "null_resource" "kubernetes" {
     host        = aws_instance.instance.public_ip
   }
 }
-# end resource
+
+resource "aws_ssm_document" "cloud_init_wait" {
+  name = "cloud-init-wait"
+  document_type = "Command"
+  document_format = "YAML"
+  content = <<-DOC
+    schemaVersion: '2.2'
+    description: Wait for cloud init to finish
+    mainSteps:
+    - action: aws:runShellScript
+      name: StopOnLinux
+      precondition:
+        StringEquals:
+        - platformType
+        - Linux
+      inputs:
+        runCommand:
+        - cloud-init status --wait
+    DOC
+}

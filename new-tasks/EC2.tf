389e0aced4f5c5d8a13ac10a33fb3f459e5b370b
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 16:04:04 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -13,40 +13,24 @@ resource "aws_instance" "instance" {
     Name = "${var.NAME}"
   }
 }
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-}
 
-#module "key_pair" {
-#  source = "terraform-aws-modules/key-pair/aws"
-#  public_key = trimspace(tls_private_key.key.public_key_openssh)
-#}
-
-resource "null_resource" "key" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      sudo echo '${tls_private_key.key.private_key_pem}' > ./'${var.NAME}'.pem
-      sudo chmod 400 ./'${var.NAME}'.pem
-    EOT
-  }
-}
 resource "aws_key_pair" "my_key" {
-  public_key = trimspace(tls_private_key.key.public_key_openssh)
+  public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
 }
 
 resource "null_resource" "kubernetes" {
   provisioner "remote-exec" {
     inline = ["cloud-init status --wait"]
   }
-    provisioner "local-exec" {
-      command = "/bin/bash setup.sh"
-    }
+  provisioner "local-exec" {
+    command = "/bin/bash setup.sh"
+  }
   # Login to the centos-user with the aws key.
   connection {
     type        = "ssh"
     user        = "centos"
     password    = ""
-    private_key = tls_private_key.key.public_key_pem
+    private_key = file(pathexpand("~/.ssh/id_rsa"))
     host        = aws_instance.instance.public_ip
   }
 }
@@ -70,3 +54,73 @@ resource "aws_ssm_document" "cloud_init_wait" {
         - cloud-init status --wait
     DOC
 }
+
+#
+#resource "aws_instance" "instance" {
+#  ami                         = var.ami
+#  instance_type               = var.type
+#  associate_public_ip_address = true
+#  key_name                    = aws_key_pair.my_key.key_name
+#  vpc_security_group_ids      = [aws_security_group.sg.id]
+#  subnet_id                   = aws_subnet.pub_subnet.*.id[0]
+#  ebs_block_device {
+#    device_name = "/dev/sda1"
+#    volume_size = 100
+#  }
+#  tags = {
+#    Name = "${var.NAME}"
+#  }
+#}
+#resource "tls_private_key" "key" {
+#  algorithm = "RSA"
+#}
+#
+#
+#resource "null_resource" "key" {
+#  provisioner "local-exec" {
+#    command = <<-EOT
+#      sudo echo '${tls_private_key.key.private_key_pem}' > ./'${var.NAME}'.pem
+#      sudo chmod 400 ./'${var.NAME}'.pem
+#    EOT
+#  }
+#}
+#resource "aws_key_pair" "my_key" {
+#  public_key = trimspace(tls_private_key.key.public_key_openssh)
+#}
+#
+#resource "null_resource" "kubernetes" {
+#  provisioner "remote-exec" {
+#    inline = ["cloud-init status --wait"]
+#  }
+#  provisioner "local-exec" {
+#    command = "/bin/bash setup.sh"
+#  }
+  # Login to the centos-user with the aws key.
+#  connection {
+#    type        = "ssh"
+#    user        = "centos"
+#    password    = ""
+#    private_key = tls_private_key.key.public_key_pem
+#    host        = aws_instance.instance.public_ip
+#  }
+#}
+#
+#resource "aws_ssm_document" "cloud_init_wait" {
+#  name = "cloud-init-wait"
+#  document_type = "Command"
+#  document_format = "YAML"
+#  content = <<-DOC
+#    schemaVersion: '2.2'
+#    description: Wait for cloud init to finish
+#    mainSteps:
+#    - action: aws:runShellScript
+#      name: StopOnLinux
+#      precondition:
+#        StringEquals:
+#        - platformType
+#        - Linux
+#      inputs:
+#        runCommand:
+#        - cloud-init status --wait
+#    DOC
+#}

cfd31050752fd623f5f07f2c64f196d8cf8f944f
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 20:56:18 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -112,7 +112,31 @@ resource "aws_instance" "instance" {
   }
 
   provisioner "remote-exec" {
-    inline = ["bash setup.sh"]
+    inline = [
+      "sudo yum install -y yum-utils"
+      "sudo yum-config-manager \
+      --add-repo \
+      https://download.docker.com/linux/centos/docker-ce.repo"
+      "sudo yum list docker-ce --showduplicates | sort -r -- to find the list of versions"
+      "sudo yum install docker-ce docker-ce-cli containerd.io --to install latest version -y"
+      "sudo systemctl start docker"
+      "sudo systemctl enable docker"
+      "sudo systemctl status docker"
+      "sudo swapoff -a"
+      "sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab"
+      "sudo yum -y install vim git curl wget kubelet kubeadm kubectl"
+      "sudo systemctl enable kubelet"
+      "kubeadm version"
+      "sudo kubeadm init"
+      "sudo rm /etc/containerd/config.toml"
+      "sudo systemctl restart containerd"
+      'sudo systemctl restart docker'
+      "sudo kubeadm init"
+      "mkdir -p $HOME/.kube"
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config"
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config"
+      "sudo kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml"
+    ]
   }
   tags = {
     Name = "${var.NAME}"

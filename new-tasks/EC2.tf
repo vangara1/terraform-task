6075c2f63d798f63a11765320b6c9f298d6fcc6f
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 14:34:54 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -5,45 +5,33 @@ resource "aws_instance" "instance" {
   key_name                    = var.key
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.pub_subnet.*.id[0]
-  tags                        = {
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 100
+  }
+  tags = {
     Name = "${var.NAME}"
   }
 }
-resource "aws_ssm_document" "cloud_init_wait" {
-  name = "cloud-init-wait"
-  document_type = "Command"
-  document_format = "YAML"
-  content = <<-DOC
-    schemaVersion: '2.2'
-    description: Wait for cloud init to finish
-    mainSteps:
-    - action: aws:runShellScript
-      name: StopOnLinux
-      precondition:
-        StringEquals:
-        - platformType
-        - Linux
-      inputs:
-        runCommand:
-        - cloud-init status --wait
-    DOC
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "fresh"
+  public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
 }
 
 resource "null_resource" "kubernetes" {
-
   provisioner "remote-exec" {
-    inline = ["cloud-init status --wait"," bash /root/terraform-task/new-tasks/setup.sh"]
-
-    }
-    }
+    inline = ["cloud-init status --wait", " bash /root/terraform-task/new-tasks/setup.sh"]
+  }
 
 
-## Login to the centos-user with the aws key.
-#connection {
-#  type        = "ssh"
-#  user        = "centos"
-#  password    = ""
-#  private_key = file(var.keyPath)
-#  host        = self.public_ip
-#}
-#} # end resource
+  # Login to the centos-user with the aws key.
+  connection {
+    type        = "ssh"
+    user        = "centos"
+    password    = ""
+    private_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+    host        = aws_instance.instance.public_ip
+  }
+}
+# end resource

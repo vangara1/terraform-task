89b5246522a9b743ef7cddae0bbe46d3515d3533
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 21:17:38 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -113,6 +113,15 @@ resource "aws_instance" "instance" {
 
   provisioner "remote-exec" {
     inline = [
+      "cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF",
       "sudo setenforce 0",
       "sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config",
       "sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes",

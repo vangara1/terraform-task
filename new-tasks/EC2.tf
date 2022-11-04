1b735cf612252d69a6b62a55e225d24a08d93771
vangara1 <vangarasandeep1996@gmail.com>
2022-11-04 15:02:48 +0530

    test run

--- new-tasks/EC2.tf
+++ new-tasks/EC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami                         = var.ami
   instance_type               = var.type
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.my_key.key_name
+  key_name                    = module.key_pair.key_name
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.pub_subnet.*.id[0]
   ebs_block_device {
@@ -13,10 +13,26 @@ resource "aws_instance" "instance" {
     Name = "${var.NAME}"
   }
 }
-
-resource "aws_key_pair" "my_key" {
-  public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
 }
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+  public_key = trimspace(tls_private_key.key.public_key_openssh)
+}
+
+resource "null_resource" "key" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      sudo echo '${tls_private_key.key.private_key_pem}' > ./'${var.NAME}'.pem
+      sudo chmod 400 ./'${var.NAME}'.pem
+    EOT
+  }
+}
+#resource "aws_key_pair" "my_key" {
+#  public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+#}
 
 resource "null_resource" "kubernetes" {
   provisioner "remote-exec" {
@@ -29,7 +45,7 @@ resource "null_resource" "kubernetes" {
     type        = "ssh"
     user        = "centos"
     password    = ""
-    private_key = file(pathexpand("~/.ssh/id_rsa"))
+    private_key = '${var.NAME}'.pem
     host        = aws_instance.instance.public_ip
   }
 }
